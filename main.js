c8513260f979a1a098904daa64b4564de5736c77
Mohammad Wasi <150406862+MohammadWasi12345@users.noreply.github.com>
2023-12-15 22:48:15 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prompt = require('prompt-sync')();
+//
+// get input from the user.
+//
+let n1 = parseInt(prompt('enter your first number: '));
+let n2 = parseInt(prompt('enter your second number :'));
+let operator = prompt('enter your operator("+","-","*","/"):  ');
+if (operator == "+") {
+    console.log(`your answer is ${n1}${operator} ${n2}=${n1 + n2}`);
+}
+else if (operator == "-") {
+    console.log(`your answer is ${n1}${operator} ${n2}=${n1 - n2}`);
+}
+else if (operator == "*") {
+    console.log(`your answer is ${n1}${operator} ${n2}=${n1 * n2}`);
+}
+else if (operator == "/") {
+    console.log(`your answer is ${n1}${operator} ${n2}=${n1 / n2}`);
+}
+else {
+    console.log('your operator is not valid');
+}
